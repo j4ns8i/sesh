c827dd4ad426425676cf3b16cf385a9e8288153d
j4ns8i <jsmalkowski+github@gmail.com>
2025-01-21 22:24:02 -0500

    feat(wip): use default session file if none provided and set up autocmds

--- lua/sesh/init.lua
+++ lua/sesh/init.lua
@@ -8,29 +8,55 @@ M.config = {
   session_file = nil,
 }
 
+function M.augroup_name()
+  return "Sesh"
+end
+
 ---@param opts Config
 function M.setup(opts)
-  M.config = vim.tbl_deep_extend("force", M.config, opts)
-  print("initializing with config: " .. vim.inspect(M.config))
-
   -- If a session file was specified, use that instead of the session_file opt
   if vim.g.SessionLoad == 1 then
     print("SessionLoad is set")
   end
 
-  if vim.v.this_session ~= "" then
-    print("session: " .. vim.v.this_session)
-    M.config.session_file = vim.v.this_session
-  end
-
-  if vim.v.this_session == "" and #vim.fn.argv() > 0 then
+  local using_session_arg = vim.v.this_session ~= ""
+  if #vim.fn.argv() > 0 and not using_session_arg then
     -- If files were given as arguments, don't activate.
-    -- Only check this if a session file wasn't specified because session files
+    -- Only check this if a session arg wasn't specified because session files
     -- are just vim scripts that add call :addarg to restore files.
+    -- TODO: activate if no existing session exists
     print("files given as arguments, exiting...")
+    return
   end
 
-  print("initialized with config: " .. vim.inspect(M.config))
+  if using_session_arg then
+    M.config.session_file = vim.v.this_session
+  else
+    local session_file = M.get_default_session_file()
+    vim.uv.fs_mkdir(vim.fs.dirname(session_file), 0755)
+    M.config.session_file = session_file
+  end
+
+  M.config = vim.tbl_deep_extend("force", M.config, opts)
+
+  local augroup_id = vim.api.nvim_create_augroup(M.augroup_name(), { clear = true })
+  vim.api.nvim_create_autocmd({
+    "BufAdd", "BufDelete",
+    "WinNew", "WinClosed",
+    "ExitPre",
+  }, {
+    group = augroup_id,
+    callback = M.save_session()
+  })
+end
+
+function M.get_default_session_file()
+  local encoded_pwd = vim.env.PWD:gsub("/", "\\%%")
+  return vim.fn.stdpath("state") .. "/sesh/" .. encoded_pwd .. ".vim"
+end
+
+function M.save_session()
+  vim.cmd("mksession! " .. M.config.session_file)
 end
 
 return M
