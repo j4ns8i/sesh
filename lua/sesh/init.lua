2d9abffd49f387597bffc442aaf4e7b7706e283b
j4ns8i <jsmalkowski+github@gmail.com>
2025-01-26 21:01:30 -0500

    feat: add on_file_arguments opt with more useful default behavior

--- lua/sesh/init.lua
+++ lua/sesh/init.lua
@@ -56,10 +56,14 @@ end
 
 ---@class Config
 ---@field session_file string The path of the Session file.
+---@field on_file_arguments string Whether to create a session if files were specified on the command line. Possible values are "never", "create", and "overwrite".
 
 ---@type Config
 M.config = get_g("config") or {
   session_file = M.get_default_session_file(),
+  -- Create a session if one doesn't exist when files were specified on command
+  -- line.
+  on_file_arguments = "create",
 }
 
 M.augroup_name = "Sesh"
@@ -72,39 +76,50 @@ local function create_user_commands()
 end
 
 local function init(opts)
-  -- If a session file was specified, use that instead of the session_file opt
-  if vim.g.SessionLoad == 1 then
-    print("SessionLoad is set")
-  end
-
-  local using_session_arg = vim.v.this_session ~= ""
-  if #vim.fn.argv() > 0 and not using_session_arg then
-    -- If files were given as arguments, don't activate.
-    -- Only check this if a session arg wasn't specified because session files
-    -- are just vim scripts that add call :addarg to restore files.
-    -- TODO: activate if no existing session exists
-    print("files given as arguments, exiting...")
-    return
-  end
+  local cmdline_used_session_arg = vim.v.this_session ~= ""
+  local cmdline_used_file_args = #vim.fn.argv() > 0 and not cmdline_used_session_arg
 
   M.config = vim.tbl_deep_extend("force", M.config, opts)
 
-  if using_session_arg then
-    -- Using vim -S takes precedence over this plugin's session_file opt
+  -- Using vim -S takes precedence over this plugin's session_file opt
+  if cmdline_used_session_arg then
     M.config.session_file = vim.v.this_session
-  else
-    if vim.uv.fs_stat(M.config.session_file) ~= nil then
-      vim.cmd("source " .. vim.fn.fnameescape(M.config.session_file))
+  end
+
+  local overwrite_session = false
+  if cmdline_used_file_args then
+    if
+      M.config.on_file_arguments == "never"
+      or (M.config.on_file_arguments == "create" and M.session_file_exists())
+      or (M.config.on_file_arguments ~= "overwrite")
+    then
+      return -- Don't activate a session
+    end
+    if M.config.on_file_arguments == "overwrite" then
+      overwrite_session = true
     end
   end
 
-  local using_default_session = M.config.session_file == M.get_default_session_file()
-  if using_default_session then
+  -- Load the session if it exists (unless overwriting)
+  if M.session_file_exists() and not overwrite_session then
+    M.load_session()
+  end
+
+  -- Otherwise create the session file dir if needed
+  if not M.session_file_exists() and vim.uv.fs_stat(vim.fs.dirname(M.config.session_file)) == nil then
     vim.uv.fs_mkdir(vim.fs.dirname(M.config.session_file), 493)
   end
 
   create_user_commands()
   M.activate()
+end
+
+function M.session_file_exists()
+  return vim.uv.fs_stat(M.config.session_file) ~= nil
+end
+
+function M.load_session()
+  vim.cmd("source " .. vim.fn.fnameescape(M.config.session_file))
 end
 
 ---@param opts Config
